4027f2fb49d4b6ed7d594124c47a55c6dce7478a
Christian Marcell de Oliveira <chroda@chroda.com.br>
2025-02-24 20:51:56 +0000

    dark-mode implemented

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-fetch("js/games.json")
+document.addEventListener("DOMContentLoaded", () => {
+  const themeToggle = document.getElementById("theme-toggle");
+  const body = document.body;
+  const currentTheme = localStorage.getItem("theme") || "dark";
+  body.classList.remove("light-mode");
+  body.classList.remove("dark-mode");
+  body.classList.add(currentTheme === "light" ? "light-mode" : "dark-mode");
+  themeToggle.addEventListener("click", () => {
+    body.classList.toggle("light-mode");
+    body.classList.toggle("dark-mode");
+    const newTheme = body.classList.contains("light-mode") ? "light" : "dark";
+    localStorage.setItem("theme", newTheme);
+    themeToggle.setAttribute(
+      "title",
+      newTheme === "light" ? "Dark Mode" : "Light Mode"
+    );
+  });
+});
+
+fetch("games.json")
   .then((response) => response.json())
   .then((games) => {
     const grid = document.getElementById("game-grid");
